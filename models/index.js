9872b6848f1d04b41d14e27e4934873fcbb5fef9
Armando <cuelarmando@yahoo.com>
2022-02-28 09:20:16 -0500

    added comments

--- models/index.js
+++ models/index.js
@@ -1,6 +1,7 @@
 const User = require('./Users');
 const Post = require('./Post');
 const Vote = require('./Vote');
+const Comment = require('./Comment');
 
 // create association
 User.hasMany(Post, {
@@ -38,4 +39,18 @@ User.hasMany(Vote, {
 Post.hasMany(Vote, {
     foreignKey: 'post_id'
 });
-module.exports = { User, Post, Vote };
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
+User.hasMany(Comment, {
+    foreignKey: 'user_id'
+});
+Post.hasMany(Comment, {
+    foreignKey: 'post_id'
+});
+
+module.exports = { User, Post, Vote, Comment };
